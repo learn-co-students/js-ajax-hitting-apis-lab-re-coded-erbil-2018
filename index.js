3762720b85af0c315db75e903b92dc42b76ad8ef
Kirmanj Faris <kirmanj.00806510@gmail.com>
2018-08-23 16:25:53 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+const rootURL = "https://api.github.com";
+
+// function getRepositories() {
+//   const name = document.getElementById("username").value
+//   const uri = rootURL + "/users/" + name + "/repos"
+//   const xhr = new XMLHttpRequest()
+//   xhr.addEventListener("load", displayRepositories)
+//   xhr.open("GET", uri)
+//   xhr.send()
+//   return false;
+// }
+
+
+function getRepositories(){
+  
+  const name = document.getElementById("username").value;
+  
+}
